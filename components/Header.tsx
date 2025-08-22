dfcd8dd0b6c9449196a8351897cf74d15bbacce7
alyssonHK <46615603+alyssonHK@users.noreply.github.com>
2025-08-22 08:21:22 -0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useAuth } from './contexts/AuthContext';
+import { useAuth } from '../contexts/AuthContext';
 
 export const Header: React.FC = () => {
   const { user, logout } = useAuth();
